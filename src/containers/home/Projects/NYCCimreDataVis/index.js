4953b1cfc4e2f8cbd7c262d30609d73a04c8c7a8
kkker <hungkaiche@gmail.com>
2019-09-30 20:39:42 -0400

    working on gh-page deployment

--- src/containers/home/Projects/NYCCimreDataVis/index.js
+++ src/containers/home/Projects/NYCCimreDataVis/index.js
@@ -1,11 +1,5 @@
 
 import React, { Component } from 'react';
-import { Route, Link, Switch } from "react-router-dom";
-import PropTypes from 'prop-types';
-import { Map, List } from 'immutable';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
-import { directive } from '@babel/types';
 import {StyledNYCCimreDataVis} from './Styled'
 import {StyledProjects} from '../Styled'
 
