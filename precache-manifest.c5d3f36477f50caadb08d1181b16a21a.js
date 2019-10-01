0ab42794883a1aa62f170db03f2457add64a35ee
kkker <hungkaiche@gmail.com>
2019-09-30 20:35:35 -0400

    Updates

---
+++ precache-manifest.c5d3f36477f50caadb08d1181b16a21a.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d53565bc204fc121a9f10a33526fe06c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f704b5176acad319f36",
+    "url": "/static/css/main.755336f7.chunk.css"
+  },
+  {
+    "revision": "9e4fbf30ea20cad55651",
+    "url": "/static/js/2.fde7b9d9.chunk.js"
+  },
+  {
+    "revision": "4f704b5176acad319f36",
+    "url": "/static/js/main.e737d8eb.chunk.js"
+  },
+  {
+    "revision": "b59dbb437bf525ebd345",
+    "url": "/static/js/runtime~main.ab0f8bc2.js"
+  },
+  {
+    "revision": "3a6c8ef6d7b08177b0e1b27c29ce5815",
+    "url": "/static/media/3DSceneParsingReconstruction00.3a6c8ef6.png"
+  },
+  {
+    "revision": "5d997680de3f5fa967a416d30432243d",
+    "url": "/static/media/deepbox.5d997680.png"
+  },
+  {
+    "revision": "af625b73eef84ff5d3a71a28d0acc9ba",
+    "url": "/static/media/depthEstimation.af625b73.png"
+  },
+  {
+    "revision": "5b24efe52e9bf1f98e1ba681ad9ca21f",
+    "url": "/static/media/detectron.5b24efe5.jpg"
+  },
+  {
+    "revision": "936914aa4b935e5a349350e2b7bb2d38",
+    "url": "/static/media/kai2.936914aa.png"
+  },
+  {
+    "revision": "26bb3a2ff3889cb62fc10cf60eb005c2",
+    "url": "/static/media/phoneOnHand.26bb3a2f.png"
+  },
+  {
+    "revision": "93945ef165b2771a6f0cc66d994d5a5d",
+    "url": "/static/media/profile.93945ef1.png"
+  },
+  {
+    "revision": "9a9f61e77ce74a10b5ee61a9eda6cf88",
+    "url": "/static/media/prototype00.9a9f61e7.gif"
+  },
+  {
+    "revision": "3f68dae90dc7c14a701e7e6d71fb901c",
+    "url": "/static/media/prototype01.3f68dae9.gif"
+  },
+  {
+    "revision": "25135e0ff838757eef6c5e3c96aed2f3",
+    "url": "/static/media/systemDiagram.25135e0f.png"
+  },
+  {
+    "revision": "6a166517e96bb26a1cc1392c5f2ef459",
+    "url": "/static/media/volume.6a166517.jpg"
+  },
+  {
+    "revision": "6cd18bc0983f441a1b55d38408a271a7",
+    "url": "/static/media/wave-bot.6cd18bc0.png"
+  },
+  {
+    "revision": "ba75e0c9c558e20911df32aa3cc7a5bd",
+    "url": "/static/media/wave-mid.ba75e0c9.png"
+  },
+  {
+    "revision": "05d52988a72cb1da374bc1690f060224",
+    "url": "/static/media/wave-top.05d52988.png"
+  }
+]);
